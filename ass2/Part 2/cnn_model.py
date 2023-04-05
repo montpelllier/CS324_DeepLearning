9294741c5c72c4196e5c9f8e71f00905c319cd45
admin <1565975426@qq.com>
2023-04-05 20:49:41 +0800

    Changes

--- ass2/Part 2/cnn_model.py
+++ ass2/Part 2/cnn_model.py
@@ -2,24 +2,40 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
+from torch import nn
+
+
 class CNN(nn.Module):
 
-  def __init__(self, n_channels, n_classes):
-    """
-    Initializes CNN object. 
-    
-    Args:
-      n_channels: number of input channels
-      n_classes: number of classes of the classification problem
-    """
+    def __init__(self, n_channels, n_classes):
+        """
+        Initializes CNN object.
 
-  def forward(self, x):
-    """
-    Performs forward pass of the input.
-    
-    Args:
-      x: input to the network
-    Returns:
-      out: outputs of the network
-    """
-    return out
+        Args:
+          n_channels: number of input channels
+          n_classes: number of classes of the classification problem
+        """
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        """
+        Performs forward pass of the input.
+
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+        """
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return out
