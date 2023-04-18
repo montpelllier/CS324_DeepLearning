c1c7970b4ca8b2f7c8cb2c3199e2f8f18677f901
admin <1565975426@qq.com>
2023-04-19 01:17:36 +0800

    rename var and add softmax

--- ass2/Part 3/vanilla_rnn.py
+++ ass2/Part 3/vanilla_rnn.py
@@ -16,24 +16,25 @@ class VanillaRNN(nn.Module):
         # create variables for weights and biases
         self.W_hx = nn.Parameter(torch.empty((hidden_dim, input_dim)).to(device), requires_grad=True)
         self.W_hh = nn.Parameter(torch.empty((hidden_dim, hidden_dim)).to(device), requires_grad=True)
-        self.W_yh = nn.Parameter(torch.empty((output_dim, hidden_dim)).to(device), requires_grad=True)
+        self.W_ph = nn.Parameter(torch.empty((output_dim, hidden_dim)).to(device), requires_grad=True)
         self.b_h = nn.Parameter(torch.empty((hidden_dim, 1)).to(device), requires_grad=True)
-        self.b_y = nn.Parameter(torch.empty((output_dim, 1)).to(device), requires_grad=True)
+        self.b_o = nn.Parameter(torch.empty((output_dim, 1)).to(device), requires_grad=True)
         self.h = nn.Parameter(torch.empty((hidden_dim, batch_size)).to(device))
-        # initialize weights and biases with xavier and zeros initialization
+        # initialize weights and biases with xavier and zeros initializations
         nn.init.xavier_uniform_(self.W_hx)
         nn.init.xavier_uniform_(self.W_hh)
-        nn.init.xavier_uniform_(self.W_yh)
+        nn.init.xavier_uniform_(self.W_ph)
         nn.init.zeros_(self.b_h)
-        nn.init.zeros_(self.b_y)
+        nn.init.zeros_(self.b_o)
 
     def forward(self, x):
-        nn.init.zeros_(self.h)  # initialize
-        h_t = self.h
-        y = None
+        # initialize
+        nn.init.zeros_(self.h)
+        h_t, o_t = self.h, None
 
         for t in range(self.seq_length):
             x_t = x[:, t].view(1, -1)
             h_t = torch.tanh(self.W_hx @ x_t + self.W_hh @ h_t + self.b_h)  # calculate hidden state
-            y = self.W_yh @ h_t + self.b_y  # calculate output
-        return y.T
+            o_t = self.W_ph @ h_t + self.b_o  # calculate output
+        y = torch.softmax(o_t.T, dim=1)
+        return y
