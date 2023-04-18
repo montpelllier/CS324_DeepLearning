3886ac399485ef28d182d59cb436073169bf1c81
montpelllier <1565975426@qq.com>
2023-04-18 18:58:39 +0800

    complete train.py, add device

--- ass2/Part 3/train.py
+++ ass2/Part 3/train.py
@@ -6,28 +6,33 @@ import argparse
 
 import numpy as np
 from torch.utils.data import DataLoader
-
+from matplotlib import pyplot as plt
 from dataset import PalindromeDataset
 from vanilla_rnn import *
 
 
 def train(config):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # Initialize the model that we are going to use
     model = VanillaRNN(seq_length=config.input_length, input_dim=config.input_dim, hidden_dim=config.num_hidden,
-                       output_dim=config.num_classes, batch_size=config.batch_size)
+                       output_dim=config.num_classes, batch_size=config.batch_size, device=device)
 
     # Initialize the dataset and data loader (leave the +1)
     dataset = PalindromeDataset(config.input_length + 1)
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
 
-    # Setup the loss and optimizer
-    criterion = nn.CrossEntropyLoss()  # fixme
-    optimizer = torch.optim.RMSprop(model.parameters(), lr=config.learning_rate)  # fixme
+    # Set up the loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=config.learning_rate)
+
+    # Create list to store data
+    acc_list = []
+    loss_list = []
 
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
-        # print(batch_inputs)
-        # print(batch_targets)
-        # Add more code here ...
+        batch_inputs = batch_inputs.to(device)
+        batch_targets = batch_targets.to(device)
+
         optimizer.zero_grad()
         output = model.forward(batch_inputs)
         loss = criterion(output, batch_targets)
@@ -35,17 +40,15 @@ def train(config):
         # the following line is to deal with exploding gradients
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.max_norm)
         optimizer.step()
-        # Add more code here ...
-        # print(output)
-        loss = loss.item()  # fixme
+
+        loss = loss.item()
         acc_in = np.argmax(output.cpu().detach().numpy(), axis=1) == batch_targets.cpu().detach().numpy()
         accuracy = np.sum(acc_in) / batch_targets.shape[0]
-        # accuracy = 0.0  # fixme
-        # print(accuracy)
+        loss_list.append(loss)
+        acc_list.append(accuracy)
 
         if step % 10 == 0:
             print(step, accuracy, loss)
-        # print acuracy/loss here
 
         if step == config.train_steps:
             # If you receive a PyTorch data-loader error, check this bug report:
@@ -53,6 +56,18 @@ def train(config):
             break
 
     print('Done training.')
+    # draw curve of acc and loss
+    plt.figure()
+    plt.title("Pytorch CNN Accuracy")
+    plt.plot(acc_list, label="train accuracy")
+    # plt.plot(test_acc_list, label="test accuracy")
+    plt.ylabel("accuracy")
+    plt.legend()
+    plt.figure()
+    plt.title("Pytorch CNN Loss")
+    plt.plot(loss_list, 'b-')
+    plt.ylabel("loss function")
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -60,7 +75,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--input_length', type=int, default=10, help='Length of an input sequence')
+    parser.add_argument('--input_length', type=int, default=5, help='Length of an input sequence')
     parser.add_argument('--input_dim', type=int, default=1, help='Dimensionality of input sequence')
     parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
     parser.add_argument('--num_hidden', type=int, default=128, help='Number of hidden units in the model')
--- ass2/Part 3/vanilla_rnn.py
+++ ass2/Part 3/vanilla_rnn.py
@@ -8,26 +8,31 @@ import torch.nn as nn
 
 class VanillaRNN(nn.Module):
 
-    def __init__(self, seq_length, input_dim, hidden_dim, output_dim, batch_size):
+    def __init__(self, seq_length, input_dim, hidden_dim, output_dim, batch_size, device):
         super(VanillaRNN, self).__init__()
         self.seq_length = seq_length
         self.hidden_dim = hidden_dim
         self.batch_size = batch_size
         # create variables for weights and biases
-        self.W_hx = nn.Parameter(torch.empty((hidden_dim, input_dim)), requires_grad=True)
-        self.W_hh = nn.Parameter(torch.empty((hidden_dim, hidden_dim)), requires_grad=True)
-        self.W_yh = nn.Parameter(torch.empty((output_dim, hidden_dim)), requires_grad=True)
-        self.b_h = nn.Parameter(torch.empty((hidden_dim, 1)), requires_grad=True)
-        self.b_y = nn.Parameter(torch.empty((output_dim, 1)), requires_grad=True)
+        self.W_hx = nn.Parameter(torch.empty((hidden_dim, input_dim)).to(device), requires_grad=True)
+        self.W_hh = nn.Parameter(torch.empty((hidden_dim, hidden_dim)).to(device), requires_grad=True)
+        self.W_yh = nn.Parameter(torch.empty((output_dim, hidden_dim)).to(device), requires_grad=True)
+        self.b_h = nn.Parameter(torch.empty((hidden_dim, 1)).to(device), requires_grad=True)
+        self.b_y = nn.Parameter(torch.empty((output_dim, 1)).to(device), requires_grad=True)
+        self.h = nn.Parameter(torch.empty((hidden_dim, batch_size)).to(device))
         # initialize weights and biases with xavier and zeros initialization
         nn.init.xavier_uniform_(self.W_hx)
         nn.init.xavier_uniform_(self.W_hh)
         nn.init.xavier_uniform_(self.W_yh)
         nn.init.zeros_(self.b_h)
         nn.init.zeros_(self.b_y)
+        print(self.W_hx.is_cuda)
+        print(self.b_y.is_cuda)
 
     def forward(self, x):
-        h_t = torch.zeros(self.hidden_dim, self.batch_size)  # initialize hidden
+        # h_t = torch.zeros(self.hidden_dim, self.batch_size)  # initialize hidden
+        nn.init.zeros_(self.h)
+        h_t = self.h
         y = None
         for t in range(self.seq_length):
             x_t = x[:, t].view(1, -1)
