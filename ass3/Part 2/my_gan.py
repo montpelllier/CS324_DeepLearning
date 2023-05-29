c53280fc06e8f2f2112c1d5fb434a75f0789ad84
admin <1565975426@qq.com>
2023-05-30 03:35:37 +0800

    decouple models from gan, update training method in my_gan

--- ass3/Part 2/my_gan.py
+++ ass3/Part 2/my_gan.py
@@ -1,210 +1,121 @@
 import argparse
 import os
 
+import numpy as np
 import torch
-import torch.nn as nn
 import torchvision.transforms as transforms
-from matplotlib import pyplot as plt
 from torchvision import datasets
 from torchvision.utils import save_image
 from tqdm import tqdm
 
+from models import *
 
-class Generator(nn.Module):
-    def __init__(self, in_channels, out_channels, base_channel):
-        super(Generator, self).__init__()
-
-        # Construct generator. You should experiment with your model,
-        # but the following is a good start:
-        #   Linear args.latent_dim -> 128
-        #   LeakyReLU(0.2)
-        #   Linear 128 -> 256
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 256 -> 512
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 512 -> 1024
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 1024 -> 768
-        #   Output non-linearity
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.base_channel = base_channel
-
-        self.conv_blocks = nn.Sequential(
-            nn.ConvTranspose2d(in_channels, base_channel * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(base_channel * 8),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.ConvTranspose2d(base_channel * 8, base_channel * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(base_channel * 4),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.ConvTranspose2d(base_channel * 4, base_channel * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(base_channel * 2),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.ConvTranspose2d(base_channel * 2, base_channel, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(base_channel),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(base_channel, base_channel, 3, 1, 0, dilation=2, bias=False),
-            nn.BatchNorm2d(base_channel),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(base_channel, out_channels, 3, 1, 1),
-            nn.Tanh()
-        )
-
-    def forward(self, z):
-        # Generate images from z
-        x = self.conv_blocks(z)
-        return x
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-class Discriminator(nn.Module):
-    def __init__(self, in_channels, base_channel):
-        super(Discriminator, self).__init__()
+def interpolations(n_samples=20):
+    samples = []
+    for _ in range(n_samples):
+        z1 = torch.FloatTensor(np.random.normal(0, 1, args.latent_dim)).to(device)
+        z2 = torch.FloatTensor(np.random.normal(0, 1, args.latent_dim)).to(device)
+        z = torch.empty(args.latent_dim, 9).to(device)
 
-        # Construct distriminator. You should experiment with your model,
-        # but the following is a good start:
-        #   Linear 784 -> 512
-        #   LeakyReLU(0.2)
-        #   Linear 512 -> 256
-        #   LeakyReLU(0.2)
-        #   Linear 256 -> 1
-        #   Output non-linearity
+        for i in range(args.latent_dim):
+            r = torch.linspace(z1[i], z2[i], 9)
+            z[i] = r
+        z.transpose_(0, 1)
+        samples.append(z)
 
-        self.in_channels = in_channels
-        self.base_channel = base_channel
-
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels, base_channel * 1, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(base_channel * 1),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(base_channel * 1, base_channel * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(base_channel * 2),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(base_channel * 2, base_channel * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(base_channel * 4),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(base_channel * 4, base_channel * 8, 3, 1, 0, bias=False),
-            nn.BatchNorm2d(base_channel * 8),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(base_channel * 8, 1, 1, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, img):
-        # return discriminator score for img
-        x = self.model(img).squeeze()
-        return x
+    return samples
 
 
-def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+def train(dataloader, discriminator, generator, optimizer_g, optimizer_d):
     criterion = torch.nn.BCELoss()
-    generator = generator.to(device)
-    discriminator = discriminator.to(device)
+    interpolation_samples = interpolations(n_samples=100)
 
-    plot_epoch = []
-    weighted_dis = []
-    weighted_gen = []
     for epoch in range(args.n_epochs):
-        weighted_dis_loss = 0
-        weighted_gen_loss = 0
         bar = tqdm(dataloader)
         for i, (imgs, _) in enumerate(bar):
-            imgs = imgs.to(device)
-            label_0 = torch.zeros(args.batch_size).to(device)
-            label_1 = torch.ones(args.batch_size).to(device)
+            optimizer_g.zero_grad()
+            optimizer_d.zero_grad()
 
+            real_imgs = imgs.cuda()
+            real_labels = torch.ones(imgs.shape[0], dtype=torch.float, device=device)
+            fake_labels = torch.zeros(imgs.shape[0], dtype=torch.float, device=device)
             # Train Generator
-            # ---------------
-            optimizer_G.zero_grad()
-            noise = torch.randn((args.batch_size, args.latent_dim)).to(device)
-            noise = noise.view(args.batch_size, args.latent_dim, 1, 1)
-            fake_image = generator(noise)
-            # fake_image = fake_image.to(device)
-            # print("fake_image.device:", fake_image.device)
-            # print("discriminator.parameters().device:", next(discriminator.parameters()).device)
-            prediction_on_fake = discriminator(fake_image)
-            gen_loss = criterion(prediction_on_fake, label_1)
-            gen_loss.backward()
-            optimizer_G.step()
+            z = torch.FloatTensor(np.random.normal(0, 1, (imgs.shape[0], args.latent_dim))).to(device)
+            fake_imgs = generator(z)
+            fake_scores = discriminator(fake_imgs)
 
+            g_loss = criterion(fake_scores, real_labels)
+            g_loss.backward()
+            optimizer_g.step()
             # Train Discriminator
-            # -------------------
-            optimizer_D.zero_grad()
-            prediction_on_real = discriminator(imgs)
-            loss_1 = criterion(prediction_on_real, label_1)
+            fake_scores = discriminator(fake_imgs.detach())
+            real_scores = discriminator(real_imgs)
 
-            prediction_on_fake = discriminator(fake_image.detach())
-            loss_0 = criterion(prediction_on_fake, label_0)
-            loss = (loss_0 + loss_1) / 2
-            loss.backward()
-            optimizer_D.step()
-
-            # print(epoch, i, gen_loss.item(), loss.item())
-            # weighted loss of generator and discriminator
-            weighted_gen_loss = weighted_gen_loss * (i / (i + 1.)) + gen_loss * (1. / (i + 1.))
-            weighted_dis_loss = weighted_dis_loss * (i / (i + 1.)) + loss * (1. / (i + 1.))
+            d_loss_fake = criterion(fake_scores, fake_labels)
+            d_loss_real = criterion(real_scores, real_labels)
+            d_loss = (d_loss_fake + d_loss_real) / 2
+            d_loss.backward()
+            optimizer_d.step()
             # Save Images
-            # -----------
             batches_done = epoch * len(dataloader) + i
-            if batches_done % args.save_interval == 0:
-                # You can use the function save_image(Tensor (shape Bx1x28x28),
-                # filename, number of rows, normalize) to save the generated
-                # images, e.g.:
-                # save_image(gen_imgs[:25],
-                #            'images/{}.png'.format(batches_done),
-                #            nrow=5, normalize=True)
-                save_image(fake_image[:25], './images/{}.png'.format(batches_done), nrow=5, normalize=True)
-                torch.save(generator, './models/G_{}'.format(batches_done))
-                torch.save(discriminator, './models/D_{}'.format(batches_done))
-        plot_epoch.append(epoch)
-        weighted_gen.append(weighted_gen_loss)
-        weighted_dis.append(weighted_dis_loss)
-        print('Epoch: {}, Generator loss: {}, Discriminator loss: {}'.format(plot_epoch[len(plot_epoch) - 1],
-                                                                             weighted_gen[len(weighted_gen) - 1],
-                                                                             weighted_dis[len(weighted_dis) - 1]))
 
-    fig1 = plt.subplot(2, 1, 1)
-    fig2 = plt.subplot(2, 1, 2)
-    fig1.plot(plot_epoch, weighted_dis, c='red', label='discriminator loss')
-    fig1.legend()
-    fig2.plot(plot_epoch, weighted_gen, c='green', label='generator loss')
-    fig2.legend()
-    plt.savefig('/home/s11711335/gan2/plot1.jpg')
-    plt.show()
-    print('Done training.')
+            print("Epoch: {}/{} | G_Loss: {:.4f} | D_Loss {:.4f} | D(x): {:.2f} | D(G(z)): {:.2f}|".format(epoch,
+                                                                                                           args.n_epochs,
+                                                                                                           g_loss,
+                                                                                                           d_loss,
+                                                                                                           real_scores.data.mean(),
+                                                                                                           fake_scores.data.mean()))
+
+            if batches_done % args.save_interval == 0:
+                # You can use the function save_image(Tensor (shape Bx1x28x28), filename, number of rows, normalize)
+                # to save the generated images, e.g.:
+                save_image(fake_imgs.unsqueeze(1)[:25], 'samples_gan/{}.png'.format(batches_done), nrow=5,
+                           normalize=True)
+
+        if epoch % 10 == 0:
+            generator.eval()
+            for i in range(len(interpolation_samples)):
+                z = interpolation_samples[i]
+                imgs = generator(z)
+                save_image(imgs.unsqueeze(1), 'interpolations_gan/epoch{}_n{}.png'.format(epoch, i), nrow=9,
+                           normalize=True)
+            generator.train()
 
 
 def main():
-    print('GAN WORKING...')
     # Create output image directory
-    os.makedirs('images', exist_ok=True)
-    os.makedirs('models', exist_ok=True)
+    os.makedirs('samples_gan', exist_ok=True)
+    os.makedirs('interpolations_gan', exist_ok=True)
+
     # load data
+    # Normalize中参数设为(0.5,)，因为MNIST数据集为灰度图而非RGB，故为一维数据
     dataloader = torch.utils.data.DataLoader(
         datasets.MNIST('./data/mnist', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
-                           # grey graph, not RGB
                            transforms.Normalize((0.5,),
                                                 (0.5,))])),
         batch_size=args.batch_size, shuffle=True)
 
     # Initialize models and optimizers
-    generator = Generator(100, 1, 128)
-    discriminator = Discriminator(1, 128)
-    optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr)
-    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
+    generator = SimpleGenerator(args.latent_dim)
+    discriminator = SimpleDiscriminator()
+    generator, discriminator = generator.to(device), discriminator.to(device)
+    optimizer_generator = torch.optim.Adam(generator.parameters(), lr=args.lr)
+    optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
+
+    # checkpoint = torch.load("mnist_generator1.pt")
+    # generator.load_state_dict(checkpoint)
 
     # Start training
-    train(dataloader, discriminator, generator, optimizer_G, optimizer_D)
-    print('GAN DONE')
+    train(dataloader, discriminator, generator, optimizer_generator, optimizer_discriminator)
+
     # You can save your generator here to re-use it to generate images for your
     # report, e.g.:
-    # torch.save(generator.state_dict(), "mnist_generator.pt")
+    torch.save(generator.state_dict(), "mnist_generator.pt")
 
 
 if __name__ == "__main__":
